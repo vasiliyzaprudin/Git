d20c74c8ef7dcb92fdbb0ae60c5f948dfe0c909c
vasiliyzaprudin <vasiliyzaprudin@gmail.com>
2025-08-16 22:38:04 +0500

    Changes

--- src/main/java/com/example/git/Newclass.java
+++ src/main/java/com/example/git/Newclass.java
@@ -3,6 +3,6 @@ package com.example.git;
 public class Newclass {
 public static void
     main(String[] args) {
-    System.out.println("Hello, Git and GitHub");
+    System.out.println("Hello, GitHub");
     }
 }
